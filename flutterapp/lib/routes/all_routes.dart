9fa23284f3744e32ea318e66ac1d6f8e92c64777
Gromo4n <ap62ap26@gmail.com>
2026-02-28 13:39:33 +0500

    base routes struct

---
+++ flutterapp/lib/routes/all_routes.dart
@@ -0,0 +1,8 @@
+const String baseUrl = "http://127.0.0.1";
+const String api_v1 = "$baseUrl/api/v1";
+const String registerUrl = "$api_v1/auth/register";
+const String authUrl = "$api_v1/auth/login";
+const String meUrl = "$api_v1/auth/me";
+const String logoutUrl = "$api_v1/logout";
+
+const String users = "$api_v1/users";
