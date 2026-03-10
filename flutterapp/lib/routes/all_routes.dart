9bab3b8b71cc44573bb1ebd9f48320a8f3d204c9
DanisNone <gubajdullindanis0@gmail.com>
2026-03-10 15:41:25 +0500

    new ip

--- flutterapp/lib/routes/all_routes.dart
+++ flutterapp/lib/routes/all_routes.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "http://62.109.7.138:8000";
+const String baseUrl = "https://gubajdullindanis0.fvds.ru";
 const String api_v1 = "$baseUrl/api/v1";
 const String registerUrl = "$api_v1/auth/register";
 const String authUrl = "$api_v1/auth/login";
@@ -11,5 +11,5 @@ const String getConversationsUrl = "$api_v1/get_conversations";
 const String getOrCreateDialogUrl = "$api_v1/get_or_create_dialog";
 const String getAllMessageUrl = "$api_v1/get_all_message";
 
-const String webSocketUrl = "ws://62.109.7.138:8000/api/v1/ws/";
+const String webSocketUrl = "wss://gubajdullindanis0.fvds.ru/api/v1/ws/";
 
