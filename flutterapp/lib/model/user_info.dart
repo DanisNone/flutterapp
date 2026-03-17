851c0ff8a62973053044443c0c3d399e2c43d62a
DanisNone <gubajdullindanis0@gmail.com>
2026-03-17 18:32:05 +0500

    change UserInfo

--- flutterapp/lib/model/user_info.dart
+++ flutterapp/lib/model/user_info.dart
@@ -4,36 +4,32 @@ class UserInfo {
   final int id;
   final String username;
   final String? avatarUrl;
+  final String? fullName;
+  final String? bio;
 
-  UserInfo({required this.id, required this.username, this.avatarUrl});
+  UserInfo({
+    required this.id,
+    required this.username,
+    this.avatarUrl,
+    this.fullName,
+    this.bio,
+  });
 
   factory UserInfo.fromJson(Map<String, dynamic> json) {
     return UserInfo(
-      id: json['user_id'] as int,
+      id: json['id'] as int,
       username: json['username'] as String,
       avatarUrl: json['avatar_url'] as String?,
+      fullName: json['full_name'] as String?,
+      bio: json['bio'] as String?,
     );
   }
 
-  factory UserInfo.fromRawJson(String json) {
-    return UserInfo.fromJson(jsonDecode(json) as Map<String, dynamic>);
-  }
-
-  // Метод для конвертации в JSON
-  Map<String, dynamic> toJson() {
-    return {'user_id': id, 'username': username, 'avatar_url': avatarUrl};
-  }
-
-  UserInfo copyWith({
-    int? id,
-    String? username,
-    String? fullName,
-    String? avatarUrl,
-  }) {
-    return UserInfo(
-      id: id ?? this.id,
-      username: username ?? this.username,
-      avatarUrl: avatarUrl ?? this.avatarUrl,
-    );
-  }
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'username': username,
+    'avatar_url': avatarUrl,
+    'full_name': fullName,
+    'bio': bio,
+  };
 }
