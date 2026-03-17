169e9b94c4e2bd0cc980a478479357a40c209153
DanisNone <gubajdullindanis0@gmail.com>
2026-03-17 15:05:34 +0500

    add google-services

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,13 +1,82 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapp/screens/auth/login_screen.dart';
-import 'package:flutterapp/theme/app_theme.dart';
 
-void main() {
+import 'package:flutterapp/theme/app_theme.dart';
+import 'package:flutterapp/screens/auth/login_screen.dart';
+
+
+// 1. Обработчик фоновых сообщений
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Обработка фонового сообщения: ${message.messageId}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Инициализация Firebase
+  await Firebase.initializeApp();
+
+  // 3. Фоновый обработчик
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
+  Future<void> setupPushNotifications() async {
+    // 4. Разрешения
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus != AuthorizationStatus.authorized) {
+      print('Нет разрешения на уведомления');
+      return;
+    }
+
+    // 5. Токен
+    String? token = await _messaging.getToken();
+    print('FCM Token: $token');
+    // TODO: отправь на сервер
+
+    // 6. Foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Foreground: ${message.notification?.title}');
+    });
+
+    // 7. Нажатие из фона
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Открыто из уведомления (background)');
+      // TODO: навигация
+    });
+
+    // 8. Запуск из killed state
+    RemoteMessage? initialMessage = await _messaging.getInitialMessage();
+    if (initialMessage != null) {
+      print('Открыто из уведомления (killed)');
+      // TODO: навигация
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
